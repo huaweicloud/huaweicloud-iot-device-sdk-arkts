bde0e19afbd85708d20c74574999dce3043d4863
fengxianjia <fengxianjia@gmail.com>
2024-12-04 14:03:09 +0800

    feat: add the reconnecting message in MessageSample

--- huaweicloud_iot_device_library/src/main/ets/requests/CommandRsp.ts
+++ huaweicloud_iot_device_library/src/main/ets/requests/CommandRsp.ts
@@ -16,7 +16,6 @@
  * 命令响应
  */
 export interface CommandRsp {
-
   /**
    * 结果码，0表示成功，其他表示失败。不带默认认为成功
    */
@@ -31,6 +30,4 @@ export interface CommandRsp {
    * 命令的响应参数，具体字段在设备关联的产品模型中定义。可选
    */
   paras?: Object;
-
-
 }
